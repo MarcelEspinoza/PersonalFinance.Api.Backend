289d037a32ccee3bebcb5e1b9ca2dcb37ddbdabe
Marcel Espinoza <mrclesp13@gmail.com>
2025-11-17 11:51:22 +0100

    migracion

--- Data/Migrations/20251117105042_DataTimeChangeLoan.Designer.cs
+++ Data/Migrations/20251117105042_DataTimeChangeLoan.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PersonalFinance.Api.Data;
 
 #nullable disable
 
-namespace PersonalFinance.Api.Migrations
+namespace PersonalFinance.Api.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251117105042_DataTimeChangeLoan")]
+    partial class DataTimeChangeLoan
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
